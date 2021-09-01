66375346c9ab0d2129769e29a89020ae293f5c4c
Norbert305 <sealsnorbert@yahoo.com>
2021-09-01 04:52:17 +0000

    project done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our", "a"];
+  let adj = ["small", "big", "quick"];
+  let noun = ["dragon", "shark", "mouse"];
+  let site = [".com", ".net", ".git"];
+
+  let mix = " ";
+
+  for (var words of pronoun) {
+    for (var action of adj) {
+      for (var animals of noun) {
+        for (var web of site) {
+          document.querySelector("#stuff").innerHTML +=
+            "<li>" + words + action + animals + web + "</li>";
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
